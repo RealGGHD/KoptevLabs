321f503061899216165ec832fb6ee32a450d25a9
Egor Koptev <george.hirshfield@gmail.com>
2025-03-19 21:52:08 +0200

    Koptev-Lab1
    Egor Koptev

--- Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93def2886c5b8cc48667161aa982e06b72c0bff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef84778cb09a437d80009de6eca927f78c128595")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
