ae529da00cc3b38c72b0743533e4218d8d950150
Egor Koptev <george.hirshfield@gmail.com>
2025-04-01 12:06:49 +0300

    Lab1
    by Egor Koptev

---
+++ Lab1/Homework/TaskOne.cs
@@ -0,0 +1,129 @@
+﻿namespace Homework;
+
+
+class Array
+{
+    //Init private fields
+    private double[] _arrayA;
+    private double[] _arrayB;
+    private double[] _arrayC;
+    private double _a;
+    private double _b;
+    private double _c;
+    //Get private double[] fields
+    public double[] ArrayA => _arrayA;
+    public double[] ArrayB => _arrayB;
+    public double[] ArrayC => _arrayC;
+    //Get and set private double fields
+    public double A
+    {
+        get { return _a; }
+        set { _a = value; }
+    }
+    public double B
+    {
+        get { return _b; }
+        set { _b = value; }
+    }
+    public double C
+    {
+        get { return _c; } 
+        set { _c = value; }
+    }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Array()
+    {
+        _arrayA = InitArray("A");
+        _arrayB = InitArray("B");
+        _arrayC = InitArrayC(_arrayA, _arrayB);
+    }
+    /// <summary>
+    /// Initialize Array A or B
+    /// </summary>
+    double[] InitArray(string name)
+    {
+        Console.Write($"Write array {name} (Like: 10.2 3 52): ");
+        string? input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new Exception("Error: Invalid input!");
+        }
+        string[] values = input.Split(' ');
+        double[] array = System.Array.ConvertAll(values, double.Parse);
+        return array;
+    }
+    /// <summary>
+    /// Initialize Array C
+    /// </summary>
+    double[] InitArrayC(double[] arrayA, double[] arrayB)
+    {
+        List<double> listB = arrayB.ToList();
+        int minBIndex = System.Array.IndexOf(arrayB, arrayB.Min()); //Left minimum index of B
+        listB.RemoveRange(0, minBIndex + 1); //Prepared B, +1 because remove everything right from min include it
+        
+        Console.Write("Write index for array C: ");
+        string? input = Console.ReadLine(); //InputIndex
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new Exception("Error: Invalid input!");
+        }
+        int inputIndex = int.Parse(input);
+        
+        List<double> listA = arrayA.ToList();
+        int minAIndex = System.Array.LastIndexOf(arrayA, arrayA.Min()); //Right minimum index of A
+        if (inputIndex > minAIndex)
+        {
+            listA = listA.GetRange(minAIndex + 1, inputIndex - 2); //Range from Min to Input 
+        } else if (inputIndex < minAIndex)
+        {
+            listA = listA.GetRange(inputIndex + 1, minAIndex - 1); //Range from Input to Min
+        }
+        else
+        {
+            return listB.ToArray(); //Return range without A
+        }
+        return listB.Concat(listA).ToArray(); //Return range with B + A
+    }
+    /// <summary>
+    /// Print Array
+    /// </summary>
+    public void PrintArray(double[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write($"{item} ");
+        }
+    }
+    /// <summary>
+    /// The sum of positive elements after the second maximum
+    /// </summary>
+    public double GroupList(double[] array)
+    {
+        List<double> temp = array.ToList();
+        double fMax = temp.Max(); //First Max
+        temp.Remove(fMax);
+        double sMax = temp.Max(); //Second Max
+        int sMaxIndex = temp.IndexOf(sMax); //Second Max Index
+        
+        List<double> list = array.ToList();
+        list.RemoveRange(0, sMaxIndex + 1); //Delete all left from second max
+        List<double> sumList = new List<double>();
+        foreach (double element in list)
+        {
+            if (element > 0) //Sum of all positive
+            {
+                sumList.Add(element);
+            }
+        }
+        return sumList.Sum();
+    }
+    /// <summary>
+    /// Calculate function
+    /// </summary>
+    public double Calculate(double a, double b, double c)
+    {
+        return (2.0 * Math.Sin(a)) + (3.0 * b * Math.Pow(Math.Cos(c), 3)) / (a + b);
+    }
+}
--- Lab1/Homework/obj/Debug/net9.0/Homework.AssemblyInfo.cs
+++ Lab1/Homework/obj/Debug/net9.0/Homework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089f9cefed9ffdbfbc6a8feb2a1e570baacba9e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6c666031e4be1c21e22d1ba37f9d701ee2d5bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
