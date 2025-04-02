922e7a3032ec911a978465a58aa3e0106168e9ab
Egor Koptev <george.hirshfield@gmail.com>
2025-04-02 21:50:44 +0300

    Lab2
    by Egor Koptev

---
+++ Lab2/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿namespace Lab2;
+
+public class BankAccount
+{
+    //Init private fields
+    private int accountNumber;
+    private string ownerName;
+    private decimal balance;
+    Random _rnd = new Random();
+    //Get and set properties
+    public string OwnerName
+    {
+        get => ownerName;
+        set => ownerName = string.IsNullOrWhiteSpace(value) ? throw new Exception("Input cannot be empty!") : value;
+    }
+    public decimal Balance
+    {
+        get => balance;
+    }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public BankAccount(string input)
+    {
+        accountNumber = _rnd.Next(0, int.MaxValue);
+        OwnerName = input;
+    }
+    /// <summary>
+    /// Deposit money to account
+    /// </summary>
+    public void Deposit(decimal amount)
+    {
+        balance += amount;
+        Console.WriteLine($"Deposit {amount} to {OwnerName}.");
+    }
+    /// <summary>
+    /// Withdraw money from account
+    /// </summary>
+    public void Withdraw(decimal amount)
+    {
+        if (balance - amount < 0)
+        {
+            Console.WriteLine("Error: Not enough money to deposit.");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine($"{ownerName} deposited {amount}.");
+        }
+    }
+    /// <summary>
+    /// Information about bank account 
+    /// </summary>
+    public void GetAccountInfo()
+    {
+        Console.WriteLine($"Account {accountNumber} belongs to {ownerName}.");
+        Console.WriteLine($"Balance: {balance}.");
+    }
+}
---
+++ Lab2/Run.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab2;
+
+class Run
+{
+    static void Main()
+    {
+        Console.Write("Enter your name for new bank account: ");
+        string name = Input();
+        BankAccount bankAccount = new BankAccount(name);
+        Console.WriteLine("Congratulations, you have a new bank account!");
+        
+        while (true)
+        {
+            Console.WriteLine("\nAvailable actions: Deposit (1), Withdraw (2), Information about account (3).");
+            Console.Write("Choose action (1/2/3): ");
+            int action = int.Parse(Input());
+            if (action == 1)
+            {
+                Console.Write("Enter amount for deposit: ");
+                bankAccount.Deposit(decimal.Parse(Input()));
+            } 
+            else if (action == 2)
+            {
+                Console.Write("Enter amount for withdraw: ");
+                bankAccount.Withdraw(decimal.Parse(Input()));
+            }
+            else if (action == 3)
+            {
+                bankAccount.GetAccountInfo();
+            }
+        }
+    }
+    static string Input()
+    {
+        string input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new Exception("Input cannot be empty!");
+        }
+        return input;
+    }
+}
