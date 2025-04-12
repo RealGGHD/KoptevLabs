c5faaba2a0b3961d38aa3e6aa9ff18d1ade6690c
Egor Koptev <george.hirshfield@gmail.com>
2025-04-12 17:43:24 +0300

    Lab3 Add
    by Egor Koptev

---
+++ Lab3/Task1/Animal.cs
@@ -0,0 +1,9 @@
+﻿//1 variant
+namespace Lab3.Task1;
+abstract class Animal
+{
+    /// <summary>
+    /// Init abstract class
+    /// </summary>
+    public abstract void MakeSound();
+}
---
+++ Lab3/Tools.cs
@@ -0,0 +1,35 @@
+﻿//1 variant
+namespace Lab3;
+using Task1;
+class Tools
+{
+    /// <summary>
+    /// Start program
+    /// </summary>
+    static void Main()
+    {
+        //Init animals
+        Animal lion = new Lion();
+        Animal elephant = new Elephant();
+        Animal bird = new Bird();
+        while (true)
+        {
+            //Input name of animal
+            string name = Input("Write the name of animal (Lion/Elephant/Bird): ");
+            if (name == "Lion") lion.MakeSound();
+            else if (name == "Elephant") elephant.MakeSound();
+            else if (name == "Bird") bird.MakeSound();
+            else Console.WriteLine("Error: Invalid input!");
+        }
+    }
+    /// <summary>
+    /// Input and verification
+    /// </summary>
+    public static string Input(string message)
+    {
+        Console.Write(message);
+        string? input = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(input)) throw new Exception("Error: Invalid input!");
+        return input;
+    }
+}
