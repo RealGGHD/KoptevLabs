03280c0c0d41d0d5f8e8e0fca2ff022f320ab524
Egor Koptev <george.hirshfield@gmail.com>
2025-04-12 16:42:00 +0300

    Lab3
    by Egor Koptev

---
+++ Lab3/Task1/Elephant.cs
@@ -0,0 +1,13 @@
+﻿//1 variant
+namespace Lab3.Task1;
+class Elephant : Animal
+{
+    /// <summary>
+    /// Override abstract class
+    /// </summary>
+    public override void MakeSound()
+    {
+        //Make specific noise
+        Console.WriteLine("Phrrooo!");
+    }
+}
