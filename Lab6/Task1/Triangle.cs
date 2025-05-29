4294488f93bab5977b9d93f23257496792eb7f17
Egor Koptev <george.hirshfield@gmail.com>
2025-05-30 01:04:57 +0300

    Lab6 Add
    by Egor Koptev

---
+++ Lab6/Task1/Triangle.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab6.Task1;
+
+public class Triangle : Polygon
+{
+    /// <summary>
+    /// Use triangle constructor
+    /// </summary>
+    public Triangle(int[] lengthsofsides, string color)
+    {
+        LengthsOfSides = lengthsofsides;
+        Color = color;
+    }
+    /// <summary>
+    /// Calculate square of triangle via Heron's formula
+    /// </summary>
+    public override double squareCalc()
+    {
+        int first = LengthsOfSides[0];
+        int second = LengthsOfSides[1];
+        int third = LengthsOfSides[2];
+        double semiperimeter = (first + second + third) / 2.0;
+        double square = Math.Sqrt(semiperimeter * (semiperimeter - first) * (semiperimeter - second) * (semiperimeter - third));
+        Square = Math.Round(square, 2);
+        return Square;
+    }
+}
--- Lab6/Tools.cs
+++ Lab6/Tools.cs
@@ -1,19 +1,102 @@
 ﻿namespace Lab6;
+using Task1;
 
 class Tools
 {
+    private const int triangleSign = 4;
+    private const int rectangleSign = 5;
+    private const int triangleNumSides = 3;
+    private const int rectangleNumSides = 4;
+    private const int triangleColorPosition = 3;
+    private const int rectangleColorPosition = 4;
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        var data = File.ReadAllLines("../../somefile.txt");
-        foreach (var line in data)
+        string[] lines = getInput("input.txt");
+        
+        List<Polygon> polygons = new List<Polygon>();
+        
+        foreach (string line in lines)
         {
-            var elements = line.Split(' ');
-            var someObject = new
+            string[] parts = line.Split(' ');
+            int numOfParts = parts.Count();
+            
+            if (numOfParts == rectangleSign)
             {
-                length1 = int.Parse(elements[0]),
-                length2 = int.Parse(elements[1]),
+                int[] lengths = new int[rectangleNumSides];
+                partsToLengths(lengths, parts, rectangleNumSides);
+                Rectangle rectangle = new Rectangle(lengths, parts[rectangleColorPosition]);
+                polygons.Add(rectangle);
             }
+            else if (numOfParts == triangleSign)
+            {
+                int[] lengths = new int[triangleNumSides];
+                partsToLengths(lengths, parts, triangleNumSides);
+                Triangle triangle = new Triangle(lengths, parts[triangleColorPosition]);
+                polygons.Add(triangle);
+            }
+        }
+        calcSquare(polygons);
+        
+        sortPolygons(polygons);
+
+        printPolygons(polygons);
+    }
+    /// <summary>
+    /// Get path of input.txt in direstory with Tools.cs
+    /// </summary>
+    static string getPath(string fileName)
+    {
+        string path = Path.Combine(AppContext.BaseDirectory, @$"..\..\..\{fileName}");
+        string fullPath = Path.GetFullPath(path);
+        return fullPath;
+    }
+    /// <summary>
+    /// Txt to array
+    /// </summary>
+    static string[] getInput(string fileName)
+    {
+        string pathToInput = getPath(fileName);
+        return File.ReadAllLines(pathToInput);
+    }
+    /// <summary>
+    /// Calculate square for all polygons
+    /// </summary>
+    static void calcSquare(List<Polygon> polygons)
+    {
+        foreach (var polygon in polygons)
+        {
+            polygon.squareCalc();
+        }
+    }
+    /// <summary>
+    /// Calculate square for all polygons
+    /// </summary>
+    static void sortPolygons(List<Polygon> polygons)
+    {
+        polygons.Sort((p1, p2) => p1.Square.CompareTo(p2.Square));
+    }
+    /// <summary>
+    /// Print polygons one by one
+    /// </summary>
+    static void printPolygons(List<Polygon> polygons)
+    {
+        int positionOfPolygon  = 1;
+        Console.WriteLine("№ | Type of figure | Square | Color");
+        foreach (var polygon in polygons)
+        {
+            polygon.Print(positionOfPolygon);
+            positionOfPolygon++;
+        }
+    }
+    /// <summary>
+    /// Print polygons one by one
+    /// </summary>
+    static void partsToLengths(int[] lengths, string[] parts, int corner)
+    {
+        for (int i = 0; i < corner; i++)
+        {
+            lengths[i] = int.Parse(parts[i]);
         }
     }
 }
