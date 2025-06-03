67e41e41689d9c168be9b91211f922e7656ecb4e
Egor Koptev <george.hirshfield@gmail.com>
2025-06-03 14:52:00 +0300

    Lab6 Add
    by Egor Koptev

---
+++ Lab6/Task1/Polygon.cs
@@ -0,0 +1,86 @@
+﻿namespace Lab6.Task1;
+
+interface IComparable
+{
+    void Print(int index);
+}
+
+public abstract class Polygon : IComparable
+{
+    //Private const fields
+    private const int maxSides = 4;
+    private const bool IgnoreUpperLowercase = true;
+    //Private fields
+    private int[] _lengthsOfSides = new int[maxSides];
+    private string _color;
+    private double _square;
+    
+    /// <summary>
+    /// Abstract method for realization in subclasses
+    /// </summary>
+    public abstract double squareCalc();
+    /// <summary>
+    /// Get and set for array of int
+    /// </summary>
+    public int[] LengthsOfSides
+    {
+        get{return _lengthsOfSides;}
+        set{_lengthsOfSides = value;}
+    }
+    /// <summary>
+    /// Get and set for color string
+    /// </summary>
+    public string Color
+    {
+        get{return _color;}
+        set{_color = value;}
+    }
+    /// <summary>
+    /// Get and set for square int
+    /// </summary>
+    public double Square
+    {
+        get{return _square;}
+        set{_square = value;}
+    }
+    /// <summary>
+    /// Print all information about specific polygon
+    /// </summary>
+    public void Print(int index)
+    {
+        if (Enum.TryParse<ConsoleColor>(_color, IgnoreUpperLowercase, out ConsoleColor consoleColor)) //Try to parse color
+        {
+            Console.ForegroundColor = consoleColor;
+            if (GetType().Name == "Triangle" && Color == "Red" && isRectangular())
+            {
+                int first = LengthsOfSides[0];
+                int second = LengthsOfSides[1];
+                int third = LengthsOfSides[2];
+                int perimeter = first + second + third;
+                Console.WriteLine($"{index} | {GetType().Name} | {_square}, Perimeter: {perimeter} | {_color}");
+            }
+            else
+            {
+                Console.WriteLine($"{index} | {GetType().Name} | {_square} | {_color}");
+            }
+            Console.ResetColor();
+        }
+    }
+    /// <summary>
+    /// Is triangle rectangular?
+    /// </summary>
+    bool isRectangular()
+    {
+        int max = LengthsOfSides.Max();
+        int min = LengthsOfSides.Min();
+        int mid = LengthsOfSides.Sum() - max - min;
+        int maxSquared = max * max;
+        int minSquared = min * min;
+        int midSquared = mid * mid;
+        if ((minSquared + midSquared) == maxSquared)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Lab6/Task1/Triangle.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab6.Task1;
+
+public class Triangle : Polygon
+{
+    /// <summary>
+    /// Use triangle constructor
+    /// </summary>
+    public Triangle(int[] lengthsofsides, string color)
+    {
+        LengthsOfSides = lengthsofsides;
+        Color = color;
+    }
+    /// <summary>
+    /// Calculate square of triangle via Heron's formula
+    /// </summary>
+    public override double squareCalc()
+    {
+        int first = LengthsOfSides[0];
+        int second = LengthsOfSides[1];
+        int third = LengthsOfSides[2];
+        int perimeter = first + second + third;
+        double halfPerimeter = perimeter / 2.0;
+        double square = Math.Sqrt(halfPerimeter * (halfPerimeter - first) * (halfPerimeter - second) * (halfPerimeter - third));
+        Square = Math.Round(square, 2);
+        return Square;
+    }
+}
