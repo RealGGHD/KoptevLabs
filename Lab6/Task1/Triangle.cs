5e862c194655c8cb882e8d011547c5ffa81b3c5c
Egor Koptev <george.hirshfield@gmail.com>
2025-06-03 13:34:09 +0300

    Lab6 Fix
    by Egor Koptev

--- Lab6/Task1/Triangle.cs
+++ Lab6/Task1/Triangle.cs
@@ -18,8 +18,9 @@ public class Triangle : Polygon
         int first = LengthsOfSides[0];
         int second = LengthsOfSides[1];
         int third = LengthsOfSides[2];
-        double semiperimeter = (first + second + third) / 2.0;
-        double square = Math.Sqrt(semiperimeter * (semiperimeter - first) * (semiperimeter - second) * (semiperimeter - third));
+        int perimeter = first + second + third;
+        double halfPerimeter = perimeter / 2.0;
+        double square = Math.Sqrt(halfPerimeter * (halfPerimeter - first) * (halfPerimeter - second) * (halfPerimeter - third));
         Square = Math.Round(square, 2);
         return Square;
     }
